d3ee22ce254a4ee4b1e5fdce5a0ea5e25f4d9dad
Luis Grangeiro <luiscarlos.fla@hotmail.com>
2016-07-30 02:07:28 -0300

    Initial commit

---
+++ EstudoEntityFramework/ProdutoAplicacao.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudoEntityFramework
+{
+    public class ProdutoAplicacao
+    {
+        public DbProduto banco { get; set; }
+
+        public ProdutoAplicacao()
+        {
+            banco = new DbProduto();
+        }
+
+        public void Cadastrar(Produto produto)
+        {
+            banco.Produtos.Add(produto);
+            banco.SaveChanges();
+        }
+
+        public IEnumerable<Produto> Listar()
+        {
+            return banco.Produtos.ToList();
+        }
+
+        public void Alterar(Produto produto)
+        {
+            Produto produtoAlterar = banco.Produtos.Where(p => p.Id == produto.Id).First();
+
+            produtoAlterar.Nome = produto.Nome;
+
+            banco.SaveChanges();
+        }
+
+        public void Deletar(int Id)
+        {
+            Produto produtoDeletar = banco.Produtos.Where(p => p.Id == Id).First();
+
+            banco.Set<Produto>().Remove(produtoDeletar);
+            banco.SaveChanges();
+
+        }
+    }
+}
---
+++ EstudoEntityFramework/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudoEntityFramework
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto novoProduto = new Produto()
+            {
+                Id = 1,
+                Nome = "Arroz"
+            };
+
+            ProdutoAplicacao app = new ProdutoAplicacao();
+
+            app.Cadastrar(novoProduto);
+
+        }
+    }
+}
